73fff108dc2a46845f16449001400a0ce4f7e6f3
lmanciagli <leandro.manciagli@gba.gob.ar>
2025-01-28 13:56:06 -0300

    Destino service

---
+++ src/app/services/destino/destino.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { delay } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DestinoService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/destinos`).pipe(delay(500));
+  }
+}
